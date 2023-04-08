7d1a759fade719923f83598c9cb7d1f024d4bb99
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-04-08 15:01:23 +0200

    corecct pixabaycode

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
     return await fetch(
-      `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+      `https://pixabay.com/api/?key=35144361-9ec03ce098d095dbb4bda03de&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
     )
       .then(async response => {
         if (!response.ok) {
